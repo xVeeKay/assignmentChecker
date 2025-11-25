8f0d3edced9059bfc9b014294830873de3db1fd7
vishalkakiyan <vishalkakiyan@gmail.com>
2025-11-25 23:27:09 +0530

    changed views path

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const path=require('path')
 const {connectDb}=require('./config/db.cfg.js')
 
 app.set("view engine",'ejs')
-app.set('views',path.join(__dirname,'views'))
+app.set('views', path.join(__dirname, 'src/views'));
 
 
 
